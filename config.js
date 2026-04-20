8cc874dcdf1c53c12004efd602dbc3bebf40b664
Javier-One <pagodirecto.ti@gmail.com>
2026-04-20 16:28:00 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="1">
+            <File firstVisibleLine="815" xOffset="0" scrollWidth="6781" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="JSON" encoding="-1" userReadOnly="no" filename="C:\Users\OswaldoRivera\Downloads\GMONEY  WALLET Payouts - STG PLIN- BETSAFE.postman_collection.json" backupFilePath="" originalFileLastModifTimestamp="-1710958530" originalFileLastModifTimestampHigh="31215248" tabColourId="-1" RTL="no" tabPinned="no" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="3156" startPos="432" endPos="432" selMode="0" offset="0" wrapCount="1" lang="JavaScript" encoding="-1" userReadOnly="no" filename="C:\Users\OswaldoRivera\OneDrive - Gothia Group\Documents\config.js" backupFilePath="" originalFileLastModifTimestamp="1464024873" originalFileLastModifTimestampHigh="31248645" tabColourId="-1" RTL="no" tabPinned="no" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="15" xOffset="0" scrollWidth="912" startPos="1334" endPos="1334" selMode="0" offset="0" wrapCount="1" lang="HTML" encoding="-1" userReadOnly="no" filename="C:\Users\OswaldoRivera\OneDrive - Gothia Group\Documents\Dev Local\index.html" backupFilePath="" originalFileLastModifTimestamp="463449577" originalFileLastModifTimestampHigh="31248605" tabColourId="-1" RTL="no" tabPinned="no" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
